3571267eb21e37cd76b62ae254f71f0afd9deab7
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2021-10-12 18:04:39 +0300

    MyFirstViewController

--- HowToUseUIViewControllerRepresentable/ContentView.swift
+++ HowToUseUIViewControllerRepresentable/ContentView.swift
@@ -21,7 +21,7 @@ struct ContentView: View {
                 Text("Click Here")
             }
             .sheet(isPresented: $showScreen) {
-                BasicUIViewControllerRepresentable()
+                BasicUIViewControllerRepresentable(labelText: "New text here...")
             }
 
         }
@@ -36,13 +36,32 @@ struct ContentView_Previews: PreviewProvider {
 
 struct BasicUIViewControllerRepresentable: UIViewControllerRepresentable {
     
+    let labelText: String
+    
     func makeUIViewController(context: Context) -> some UIViewController {
-        let vc = UIViewController()
-        vc.view.backgroundColor = .blue
-        return vc
+        
+        return MyFirstViewController()
     }
     
     func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
         
+    }
+}
+
+class MyFirstViewController: UIViewController {
+    
+    var labelText: String = "Starting value"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .blue
+        
+        let label = UILabel()
+        label.text = labelText
+        label.textColor = UIColor.white
+        
+        view.addSubview(label)
+        label.frame = view.frame
     }
 }
