8f8aab0363607ae69b5204e3cba0733f9f66ab87
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2021-10-12 17:52:48 +0300

    Initial Commit

---
+++ HowToUseUIViewControllerRepresentable/HowToUseUIViewControllerRepresentableApp.swift
@@ -0,0 +1,17 @@
+//
+//  HowToUseUIViewControllerRepresentableApp.swift
+//  HowToUseUIViewControllerRepresentable
+//
+//  Created by ramil on 12.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct HowToUseUIViewControllerRepresentableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
